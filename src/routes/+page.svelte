8d56a7a9c77a212a9563806201f18977e72b3448
Syahrul Anuar <syahrlanuar95@gmail.com>
2023-02-26 12:22:47 +0800

    add fonts and proper styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-
+<main class="min-h-screen w-screen flex flex-col items-center justify-center gap-8 bg-gray">
+	<h1
+		class="font-tiny font-[80] text-center text-7xl md:text-8xl lg:text-9xl tracking-wide text-asparagus"
+	>
+		Syahrul Anuar
+	</h1>
+	<h2 class="font-spacegrotesk font-semibold text-xl md:text-2xl lg:text-3xl tracking-tight">
+		New Site Under Construction.
+	</h2>
+</main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {}
-	},
-	plugins: []
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                gray: '#D9D9D9',
+                asparagus: '#8DAA57',
+                'eerie-black': '#181818',
+                'mauve-taupe': '#846075',
+                'red-purple': '#AF5D63'
+            },
+            fontFamily: {
+                tiny: ['TINY'],
+                spacegrotesk: ['Space Grotesk']
+            }
+        }
+    },
+    plugins: []
 };
