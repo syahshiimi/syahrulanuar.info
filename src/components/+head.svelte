a5b3cac31a0699486d20b8e8dce1c9a23ab088c3
Syahrul Anuar <syahrlanuar95@gmail.com>
2023-04-26 10:04:16 +0800

    feat: add header tags for seo

--- src/components/+head.svelte
+++ src/components/+head.svelte
@@ -1,4 +1,46 @@
 <svelte:head>
+	<!-- Primary Meta Tags -->
 	<title>Syahrul Anuar</title>
-	<meta title="Syahrul Anuar" />
+	<meta
+		name="title"
+		content="Syahru Anuar is an artist
+    and cultural worker"
+	/>
+	<meta
+		name="description"
+		content="Syahrul Anuar (b. 1995, Singapore) is an artist and cultural worker whose practice moves tangential to the interests of merantau (migration), owing to his family’s diaspora within the Nusantara (Malay World)."
+	/>
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://syahrulanuar.info" />
+	<meta
+		property="og:title"
+		content="Syahrul Anuar is an
+    aritst and cultural worker"
+	/>
+	<meta
+		property="og:description"
+		content="Syahrul Anuar is an
+    aritst and cultural worker"
+	/>
+	<meta property="og:image" content="/seo/seo_image.jpg" />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="Syahrul Anuar" />
+	<meta property="twitter:url" content="https://syahrulanuar.info" />
+	<meta
+		property="twitter:title"
+		content="Syahrul Anuar is
+    an artist and cultural worker"
+	/>
+	<meta
+		property="twitter:description"
+		content="Syahrul Anuar (b. 1995, Singapore) is an artist and cultural worker whose practice moves tangential to the interests of merantau (migration), owing to his family’s diaspora within the Nusantara (Malay World)."
+	/>
+	<meta property="twitter:image" content="/seo/seo_image.jpg" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+	<link rel="manifest" href="/favicons/site.webmanifest" />
 </svelte:head>
